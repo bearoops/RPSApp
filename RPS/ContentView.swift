2dc002ff15fef704e3a26b73fd260d925a35834c
Bear <164508906@qq.com>
2026-02-14 15:03:21 +0800

    Initial Commit

---
+++ RPS/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  RPS
+//
+//  Created by issuser on 2026/2/14.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ RPS/RPSApp.swift
@@ -0,0 +1,17 @@
+//
+//  RPSApp.swift
+//  RPS
+//
+//  Created by issuser on 2026/2/14.
+//
+
+import SwiftUI
+
+@main
+struct RPSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
